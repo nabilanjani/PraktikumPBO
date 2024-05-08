8120414195d5433ce55a6cdbec0ab24e04f14cb7
nabilanjani <160628861+nabilanjani@users.noreply.github.com>
2024-05-08 21:54:08 +0700

    Delete PRAKTIKUM7_MODUL directory

--- PRAKTIKUM7_MODUL/src/Vehicle.java
+++
@@ -1,6 +0,0 @@
-public class Vehicle {
-    void calRent (int distance, float price){
-        float fare = distance * price;
-        System.out.println("Vehicle price = " + fare);
-    }
-}
--- PRAKTIKUM7_MODUL/src/inclusion2/Payroll.java
+++
@@ -1,7 +0,0 @@
-package inclusion2;
-
-public class Payroll {
-    public void cetakGaji(Pegawai p){
-        p.tampilData();
-    }
-}
